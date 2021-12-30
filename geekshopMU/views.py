f06cb4754f3a4eb9304aacb4bde0c37461ffe09a
Vitaliy <vitaliy2021dubinin@mail.ru>
2021-12-30 16:27:49 +0500

    add_lesson_6

--- geekshopMU/views.py
+++ geekshopMU/views.py
@@ -4,9 +4,13 @@ from mainapp.models import Product
 def index(request):
     title = 'Магазин атрибутики'
     products = Product.objects.all()[:3]
+    basket = []
+    if request.user.is_authenticated:
+        basket = Basket.objects.filter(user=request.user)
     context = {
         'title': title,
         'products': products,
+        'basket': basket,
     }
     return render(request, 'geekshopMU/index.html', context=context)
 
